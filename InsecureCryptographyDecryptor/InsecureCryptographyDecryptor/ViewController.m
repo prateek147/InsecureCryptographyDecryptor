1269157f70eb44192105709b7445cc355392fa4f
Prateek Gianchandani <prateek.searchingeye@gmail.com>
2014-01-14 23:07:54 +0530

    Implementation added

--- InsecureCryptographyDecryptor/InsecureCryptographyDecryptor/ViewController.m
+++ InsecureCryptographyDecryptor/InsecureCryptographyDecryptor/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "RNDecryptor.h"
 
 @interface ViewController ()
 
@@ -17,7 +18,25 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self decryptPassword];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+-(void)decryptPassword {
+      NSString *dataPath = [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0] stringByAppendingPathComponent:@"/secret-data"];
+      NSError *error;
+      NSData *encryptedData = [NSData dataWithContentsOfFile:dataPath];
+      NSData *decryptedData = [RNDecryptor decryptData:encryptedData
+                                        withPassword:@"Secret-Key"
+                                               error:&error];
+     NSString *password = [[NSString alloc] initWithData:decryptedData
+                                               encoding:NSUTF8StringEncoding];
+    
+    
+    UILabel *newLabel = [[UILabel alloc] initWithFrame:CGRectMake(50.0, 160.0, 200.0, 100.0)];
+    [self.view addSubview:newLabel];
+    [newLabel setText:password];
+
 }
 
 - (void)didReceiveMemoryWarning
