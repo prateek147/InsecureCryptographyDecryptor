0fcad809a96f869d3801608121db61dd80400d49
Prateek Gianchandani <prateek.searchingeye@gmail.com>
2014-01-14 21:46:54 +0530

    Initial commit

---
+++ InsecureCryptographyDecryptor/InsecureCryptographyDecryptor/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  InsecureCryptographyDecryptor
+//
+//  Created by Prateek Gianchandani on 1/14/14.
+//  Copyright (c) 2014 HighAltitudeHacks.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ InsecureCryptographyDecryptor/InsecureCryptographyDecryptorTests/InsecureCryptographyDecryptorTests.m
@@ -0,0 +1,34 @@
+//
+//  InsecureCryptographyDecryptorTests.m
+//  InsecureCryptographyDecryptorTests
+//
+//  Created by Prateek Gianchandani on 1/14/14.
+//  Copyright (c) 2014 HighAltitudeHacks.com. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface InsecureCryptographyDecryptorTests : XCTestCase
+
+@end
+
+@implementation InsecureCryptographyDecryptorTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
